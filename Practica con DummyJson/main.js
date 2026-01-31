a3525b82d3f3de54e442cb712b72688d1b44448a
20241062-ui <20241062@uthh.edu.mx>
2026-01-31 13:09:20 -0600

    Correcciones

--- Practica con DummyJson/main.js
+++ Practica con DummyJson/main.js
@@ -1,26 +1,22 @@
-const urlApi = "https://dummyjson.com/products";
+const urlApi = "https://dummyjson.com/products?limit=0";
+let todosLosProductos = [];
 
 const cargarProductos = () => {
     fetch(urlApi)
         .then(respuesta => respuesta.json())
         .then(data => {
-            const productos = data.products;
-            
-            console.log("productos recibidos:", productos);
-            mostrarProductos(productos);
+            todosLosProductos = data.products;
+            mostrarProductos(todosLosProductos);
         })
-        .catch(error => {
-            console.error("Error al cargar:", error);
-            alert("Error al cargar la API de DummyJSON.");
-        });
+        .catch(error => console.error("Error al cargar:", error));
 }
 
 const mostrarProductos = (productos) => {
     const contenedor = document.getElementById("contenedor-productos");
     contenedor.innerHTML = "";
 
-    if (!productos || productos.length === 0) {
-        contenedor.innerHTML = "<p>No se encontraron productos disponibles</p>";
+    if (productos.length === 0) {
+        contenedor.innerHTML = "<p>No se encontraron productos que coincidan con tu búsqueda.</p>";
         return;
     }
 
@@ -32,21 +28,33 @@ const mostrarProductos = (productos) => {
         <div class="card-content">
             <h3 class="titulo">${producto.title}</h3>
             <img src="${producto.thumbnail}" alt="${producto.title}">
-            
             <div class="detalles">
                 <p class="precio">$${producto.price}</p>
                 <span class="badge cat">${producto.category}</span>
             </div>
-
             <div class="rating-info">
                 <p class="rating">⭐ ${producto.rating}</p>
             </div>
-
             <a href="detalle.html?id=${producto.id}" class="btn-detalle">Ver Detalles</a>
         </div>
         `;
         contenedor.appendChild(tarjeta);
     });
 }
+
+const inputBusqueda = document.getElementById("input-busqueda");
+
+inputBusqueda.addEventListener("input", () => {
+    const texto = inputBusqueda.value.toLowerCase().trim();
+
+    if (texto === "") {
+        mostrarProductos(todosLosProductos);
+    } else {
+        const filtrados = todosLosProductos.filter(p => 
+            p.title.toLowerCase().includes(texto)
+        );
+        mostrarProductos(filtrados);
+    }
+});
 
 document.addEventListener('DOMContentLoaded', cargarProductos);
